e23a5b353788575b010397dd316c215b34509b89
Martijn Kunstman <martijn@kunstman.nl>
2023-04-12 22:32:36 +0200

    multirule2

--- script.js
+++ script.js
@@ -1,67 +1,79 @@
-
-let wh = 600;
-let d = 0;
-let x = Math.round(wh / 2);
-let y = Math.round(wh / 2);
+let dimension = 900;
+let direction = 0;
+let x = Math.round(dimension / 2);
+let y = Math.round(dimension / 2);
 let counterElement = document.getElementById("counter");
 let counter = 0;
 let stepsAtOnce = 1000;
 
-let dn = Date.now();
-  
+let rules = "LRRRRRLLR";
+rules = "LR";
+rules = "LLRRRLRLRLLR";
+rules = "RRLLLRLLLRRR";
+convertRules();
+
+let dateNow = Date.now();
+
 const canvas = document.getElementById("myCanvas");
-canvas.width = wh;
-canvas.height = wh;
-const ctx = canvas.getContext("2d", { alpha: false, willReadFrequently:true });
+canvas.width = dimension;
+canvas.height = dimension;
+const ctx = canvas.getContext("2d", { alpha: false, willReadFrequently: true });
 ctx.fillStyle = "blue";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+function convertRules() {
+  rules = rules.split("");
+  for (let i = 0; i < rules.length; i++) {
+    if (rules[i] == "L") {
+      rules[i] = -1;
+    } else if (rules[i] == "R") {
+      rules[i] = 1;
+    }
+  }
+}
+
 function step() {
-  let id = ctx.getImageData(0, 0, wh, wh);
+  let id = ctx.getImageData(0, 0, dimension, dimension);
   let pixels = id.data;
   for (let a = 0; a < stepsAtOnce; a++) {
-    if (pixels[x * 4 + y * wh * 4] == 255) {
-      pixels[x * 4 + y * wh * 4] = 0;
-      pixels[x * 4 + y * wh * 4 + 1] = 0;
-      pixels[x * 4 + y * wh * 4 + 2] = 0;
-      d++;
-      if (d > 3) {
-        d = 0;
-      }
-    } else {
-      pixels[x * 4 + y * wh * 4] = 255;
-      pixels[x * 4 + y * wh * 4 + 1] = 255;
-      pixels[x * 4 + y * wh * 4 + 2] = 255;
-      d--;
-      if (d < 0) {
-        d = 3;
-      }
+    
+    direction = direction + rules[pixels[x * 4 + y * dimension * 4]];
+    pixels[x * 4 + y * dimension * 4] = pixels[x * 4 + y * dimension * 4] + 1;
+    if (pixels[x * 4 + y * dimension * 4] == rules.length) {
+      pixels[x * 4 + y * dimension * 4] = 0;
     }
-    if (d == 0) {
+    pixels[x * 4 + y * dimension * 4 + 1] = 255*pixels[x * 4 + y * dimension * 4]/(rules.length-1);
+    pixels[x * 4 + y * dimension * 4 + 2] = 255*pixels[x * 4 + y * dimension * 4]/(rules.length-1);
+
+    if (direction > 3) {
+      direction = 0;
+    } else if (direction < 0) {
+      direction = 3;
+    }
+    if (direction == 0) {
       x = x - 1;
       if (x < 0) {
-        x = wh - 1;
+        x = dimension - 1;
       }
-    } else if (d == 1) {
+    } else if (direction == 1) {
       y = y - 1;
       if (y < 0) {
-        y = wh - 1;
+        y = dimension - 1;
       }
-    } else if (d == 2) {
+    } else if (direction == 2) {
       x = x + 1;
-      if (x == wh) {
+      if (x == dimension) {
         x = 0;
       }
-    } else if (d == 3) {
+    } else if (direction == 3) {
       y = y + 1;
-      if (y == wh) {
+      if (y == dimension) {
         y = 0;
       }
     }
     counter++;
-     if (counter==1000000)
-    {
-      document.getElementById("time").innerHTML=Date.now()-dn;//10958
+    if (counter == 1000000) {
+      document.getElementById("time").innerHTML = Date.now() - dateNow;
     }
   }
   ctx.putImageData(id, 0, 0);
