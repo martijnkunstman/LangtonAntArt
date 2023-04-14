aaf92d692e077c08ca018426e1110fa2ee9acea1
Martijn Kunstman <martijn@kunstman.nl>
2023-04-14 21:40:02 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let dimension = 1200;
+let dimension = 1000;
 let direction = 0;
-//let direction2 = 0;
 let x = Math.round(dimension / 2);
 let y = Math.round(dimension / 2);
-//let x2 = Math.round(dimension / 2);
-//let y2 = Math.round(dimension / 2);
 let stepsAtOnce = 10000;
+let rules = "LRRRRRLLR";
+convertRules();
 
 let rulesSelect = document.getElementById("rules");
 rulesSelect.addEventListener("change", function () {
@@ -29,6 +28,7 @@ dimensionSelect.addEventListener("change", function () {
   y = Math.round(dimension / 2);
   direction = 0;
 });
+
 let fade = true;
 let fadeSelect = document.getElementById("fader");
 fadeSelect.addEventListener("change", function () {
@@ -44,28 +44,12 @@ stepsSelect.addEventListener("change", function () {
   stepsAtOnce = stepsSelect.value;
 });
 
-let rules = "LRRRRRLLR";
-rules = "LR";
-rules = "LLRRRLRLRLLR";
-rules = "RRLLLRLLLRRR";
-convertRules();
-
 const canvas = document.getElementById("myCanvas");
 canvas.width = dimension;
 canvas.height = dimension;
 const ctx = canvas.getContext("2d", { alpha: false, willReadFrequently: true });
 ctx.fillStyle = "blue";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//const canvas2 = document.getElementById("myCanvas2");
-//canvas2.width = dimension;
-//canvas2.height = dimension;
-//const ctx2 = canvas2.getContext("2d", {
-//  alpha: false,
-//  willReadFrequently: true,
-//});
-//ctx2.fillStyle = "blue";
-//ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 
 function convertRules() {
   rules = rules.split("");
@@ -124,66 +108,7 @@ function step() {
     ctx.fillStyle = "rgba(0,0,225,0.01)";
     ctx.fillRect(0, 0, dimension, dimension);
   }
-  //step2();
   requestAnimationFrame(step);
 }
-/*
-let counter2 = 0;
-let grow = true;
-function step2() {
-  let id2 = ctx2.getImageData(0, 0, dimension, dimension);
-  let pixels = id2.data;
-  for (let a = 0; a < stepsAtOnce; a++) {
-    if (grow) {
-      counter2+=1/(stepsAtOnce/8);
-    } else {
-      counter2-=1/(stepsAtOnce/8);
-    }
-    if (counter2 >=255) {
-      grow = false;
-    }
-    if (counter2 <= 0) {
-      grow = true;
-    }
-    direction2 = direction2 + rules[pixels[x2 * 4 + y2 * dimension * 4]];
-    pixels[x2 * 4 + y2 * dimension * 4] =
-      pixels[x2 * 4 + y2 * dimension * 4] + 1;
-    if (pixels[x2 * 4 + y2 * dimension * 4] == rules.length) {
-      pixels[x2 * 4 + y2 * dimension * 4] = 0;
-    }
-    pixels[x2 * 4 + y2 * dimension * 4 + 1] = 255-counter2;
-    pixels[x2 * 4 + y2 * dimension * 4 + 2] = counter2;
 
-    if (direction2 > 3) {
-      direction2 = 0;
-    } else if (direction2 < 0) {
-      direction2 = 3;
-    }
-    if (direction2 == 0) {
-      x2 = x2 - 1;
-      if (x2 < 0) {
-        x2 = dimension - 1;
-      }
-    } else if (direction2 == 1) {
-      y2 = y2 - 1;
-      if (y2 < 0) {
-        y2 = dimension - 1;
-      }
-    } else if (direction2 == 2) {
-      x2 = x2 + 1;
-      if (x2 == dimension) {
-        x2 = 0;
-      }
-    } else if (direction2 == 3) {
-      y2 = y2 + 1;
-      if (y2 == dimension) {
-        y2 = 0;
-      }
-    }
-  }
-  ctx2.putImageData(id2, 0, 0);
-  ctx2.fillStyle = "rgba(0,0,225,0.01)";
-  ctx2.fillRect(0, 0, dimension, dimension);
-}
-*/
 step();
